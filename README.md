b84a9911cc792d75205574f22bd603792daa5f09
Prince Wilson <maxcell.wilson@gmail.com>
2022-02-05 21:33:13 -0500

    wip: Testing if imports cause it to fail

---
+++ README.md
@@ -0,0 +1,4 @@
+# Now Playing
+
+Here's an experiment of a project I've had where I wanted to
+build a now playing part of my Twitter background.
--- netlify/functions/now-playing.js
+++ netlify/functions/now-playing.js
@@ -1,35 +1,38 @@
-const {getSecrets} = require("@netlify/functions");
-const SpotifyWebApi = require("spotify-web-api-node")
+// const {getSecrets, schedule} = require("@netlify/functions");
+// const SpotifyWebApi = require("spotify-web-api-node")
 
 module.exports.handler = async function handler(event, context) {
-
-  const spotifyApi = new SpotifyWebApi();
-
-  const secrets = await getSecrets(event);
-  if(secrets.spotify) {
-    spotifyApi.setAccessToken(secrets.spotify.bearerToken)
-
-    const currentTrack = await spotifyApi.getMyCurrentPlayingTrack()
-
-    const response = {
-      href: currentTrack.body.item.href,
-      name: currentTrack.body.item.name
-    }
-
-    if(currentTrack) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(response)
-      } 
-    }
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "failed to read playing track"})
-    }
-  } else {
-    return {
-      statusCode: 401,
-      body: "You can't access this"
-    }
+  return {
+    statusCode: 200,
+    body: "ok"
   }
+  // const spotifyApi = new SpotifyWebApi();
+
+  // const secrets = await getSecrets(event);
+  // if(secrets.spotify) {
+  //   spotifyApi.setAccessToken(secrets.spotify.bearerToken)
+
+  //   const currentTrack = await spotifyApi.getMyCurrentPlayingTrack()
+
+  //   const response = {
+  //     href: currentTrack.body.item.href,
+  //     name: currentTrack.body.item.name
+  //   }
+
+  //   if(currentTrack) {
+  //     return {
+  //       statusCode: 200,
+  //       body: JSON.stringify(response)
+  //     } 
+  //   }
+  //   return {
+  //     statusCode: 400,
+  //     body: JSON.stringify({ error: "failed to read playing track"})
+  //   }
+  // } else {
+  //   return {
+  //     statusCode: 401,
+  //     body: "You can't access this"
+  //   }
+  // }
 }
